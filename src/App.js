0f09f69ec8aae21bbd46ca60dd2e98e12badb836
sasukemiyazawa <b1020251@fun.ac.jp>
2022-11-17 23:53:41 +0900

    実装

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import './App.css'
+import Getdata from './components/Getdata'
+import Senddata from './components/Senddata'
 
 function App() {
+  const baseURL = "http://localhost:3001/api/v1/"
   return (
     <div className="App">
+      <Getdata baseURL={baseURL}/>
+      <br/>
+      <br/>
 
+      <Senddata baseURL={baseURL}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
