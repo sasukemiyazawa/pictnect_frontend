986ba7c363133dd24d6b8ff09dc3af326e06b9e5
nogami <root@DESKTOP-6BTL2AM.localdomain>
2022-12-06 00:49:45 +0900

    投稿フォームに機能も実装した

--- src/components/manageEvent/EventCreate.js
+++ src/components/manageEvent/EventCreate.js
@@ -1,7 +1,12 @@
 import { useState } from 'react'
 import { useCallback } from "react"
 import axios from 'axios'
-import { Button, DialogContent, DialogContentText, DialogActions, DialogTitle, TextField, Divider, Box} from '@mui/material'
+import styled from "styled-components"
+import {
+  Button, DialogContent, DialogActions,
+  DialogTitle, TextField, Divider, Box, Input
+} from '@mui/material'
+import PhotoSizeSelectActualOutlinedIcon from '@mui/icons-material/PhotoSizeSelectActualOutlined';
 
 const EventCreate = ({ baseURL, handler }) => {
   const [eventname, setEventname] = useState('')
@@ -12,9 +17,17 @@ const EventCreate = ({ baseURL, handler }) => {
 
   //画像入力部分
   const [image, setImage] = useState('')
+  const [imageSrc, setImageSrc] = useState('')
   const selectImage = useCallback((e) => {
-    const selectImage = e.target.files[0]
-    setImage(selectImage)
+    const files = e.target.files
+    if (selectImage.length > 0) {
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        setImage(files[0])
+        setImageSrc(e.target.result)
+      }
+      reader.readAsDataURL(files[0])
+    }
   }, [])
 
 
@@ -57,30 +70,53 @@ const EventCreate = ({ baseURL, handler }) => {
   }
 
   return (
-    <Box sx={{width: 600, height: 'auto'}}>
+    <Box sx={{ height: 'auto' }}>
       <DialogTitle>イベント作成</DialogTitle>
       <DialogContent>
-        <Divider sx={{my: 1}}/>
-        <TextField id='eventname' autoFocus label="イベント名" variant="standard" margin="dense" fullWidth onChange={e => setEventname(e.target.value)}/>
-        <TextField id='term' autoFocus label="期間" variant="standard" margin="dense" fullWidth onChange={e => setTerm(e.target.value)}/>
+        <Box display='flex' sx={{ width: "100%", flexDirection: 'column' }}>
+          {
+            image ?
+              <Preview src={imageSrc} alt='Preview' />
+              :
+              <PhotoSizeSelectActualOutlinedIcon sx={{ mx: 'auto', fontSize: 150 }} />
+          }
+          <Button variant='contained' component="label" sx={{ mx: 'auto' }}>
+            <input type="file" accept='image/*' hidden onChange={e => selectImage(e)} />
+            画像を選択
+          </Button>
+        </Box>
+        <Divider sx={{ my: 1 }} />
+        <TextField id='eventname' autoFocus label="イベント名" variant="standard" margin="dense" fullWidth onChange={e => setEventname(e.target.value)} />
+        <TextField id='term' autoFocus label="期間" variant="standard" margin="dense" fullWidth onChange={e => setTerm(e.target.value)} />
         <TextField id='tag' autoFocus label="タグ" variant="standard" margin="dense" fullWidth onChange={e => setTag(e.target.value)}
-         onKeyDown={e => {if(e.key === 'Enter'){
-          addTag()
-          document.getElementById('tag').value = ''
-          }}}/>
-         {tags.map((value, index) => (
-              tag !== '' &&
-              <Box sx={{display: 'inline-block', border: 1, borderColor: "#D76B6B", borderRadius: 3, mx: 0.5, px: 1}} key={index}>{value}</Box>
-            ))}
-        <Divider sx={{my: 1}}/>
-        <TextField id='contents' autoFocus label="イベント詳細" variant="standard" margin="dense" fullWidth onChange={e => setContents(e.target.value)}/>
+          onKeyDown={e => {
+            if (e.key === 'Enter') {
+              addTag()
+              document.getElementById('tag').value = ''
+            }
+          }} />
+        {tags.map((value, index) => (
+          tag !== '' &&
+          <Box onClick={() => deleteTag(value)}
+            sx={{ display: 'inline-block', border: 1, borderColor: "#D76B6B", borderRadius: 3, mx: 0.5, px: 1 }} key={index}>
+            {value}
+          </Box>
+        ))}
+        <Divider sx={{ my: 1 }} />
+        <TextField id='contents' autoFocus label="イベント詳細" variant="standard" multiline maxRows={4} margin="dense" fullWidth onChange={e => setContents(e.target.value)} />
       </DialogContent>
       <DialogActions>
-        <Button variant='contained' onClick={()=>handler(false)}>キャンセル</Button>
-        <Button variant='contained' onClick={()=>{handler(false)}}>イベント作成</Button>
+        <Button variant='contained' onClick={() => { handler(false) }}>キャンセル</Button>
+        <Button variant='contained' onClick={() => { handler(false); sendFormData() }}>イベント作成</Button>
       </DialogActions>
     </Box>
   )
 }
 
 export default EventCreate
+
+const Preview = styled.img`
+  width: auto;
+  max-height: 300px;
+  object-fit: scale-down;
+`
