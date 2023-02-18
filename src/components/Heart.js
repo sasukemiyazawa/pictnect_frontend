216a1c49ca469197cf0544f05a56ac45896b987a
sasukemiyazawa <b1020251@fun.ac.jp>
2023-02-19 01:24:36 +0900

    Update Heart.js

--- src/components/Heart.js
+++ src/components/Heart.js
@@ -4,7 +4,7 @@ FIXME: バグだらけ
 
 import Lottie from "lottie-react"
 import { useEffect, useRef, useState } from "react"
-import hearts from "./hearts2.json"
+import hearts from "./hearts.json"
 const Heart = ({on}) => {
   const lottieRef = useRef()
   const style = {
@@ -12,8 +12,7 @@ const Heart = ({on}) => {
     width: "56.25vh",
     position: "absolute",
     top: "0px",
-    zIndex: "100",
-    color: "#F06CAA"
+    zIndex: "100"
   }
   useEffect(() => {
     if (on) {
